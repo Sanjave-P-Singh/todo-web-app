3c9d7e9751e29fb765cbef1d797186bcd33bbd79
Sanjave P. Singh <60440140+Sanjave-P-Singh@users.noreply.github.com>
2023-09-24 14:19:27 -0400

    Update web_gui.py

--- web_gui.py
+++ web_gui.py
@@ -11,8 +11,8 @@ def add_todo():
 
 
 st.title("My Todo Application")
-st.subheader("This is my todo app")
-st.write("This app is to increase your productivity")
+st.subheader("This is a list app")
+st.write("Enter an item and click the checkbox to delete when task is complete:")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
